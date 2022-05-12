987a9ee6220ed6e8e949aadeb6da2e711037aa87
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-05-12 21:06:45 +0300

    First commit

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int a [] = new int[]{20,15,20,14,7,8,60};
         for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+ " ");
+            System.out.print(a[i]+" ");
         }
     }
 }

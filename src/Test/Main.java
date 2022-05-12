50729b47e35a0ed1fb85c14a2504612ab102d0d1
Qwert_Vasiley <O.Qwert.O@gmail.com>
2022-05-12 21:32:50 +0300

    first lesson by Qwert

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -2,9 +2,9 @@ package Test;
 
 public class Main {
     public static void main(String[] args) {
-        int a [] = new int[]{20,15,20,14,7,8,60};
+        int a[] = new int[]{20, 15, 20, 14, 7, 8, 60};
         for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+" ");
+            System.out.print(a[i] + " ");
         }
     }
 }

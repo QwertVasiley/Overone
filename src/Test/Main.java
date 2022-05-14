940699bb5a2dfe19bbfa41014d8f91a9297ffe83
Qwert <hfl.r@mail.ru>
2022-05-14 13:50:27 +0300

    by Qwert

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -2,9 +2,12 @@ package Test;
 
 public class Main {
     public static void main(String[] args) {
+        int temp = 2;
+        temp >>= 1;  //?
         int a[] = new int[]{20, 15, 20, 14, 7, 8, 60};
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " FF ");
         }
+        System.out.println(temp);
     }
 }
